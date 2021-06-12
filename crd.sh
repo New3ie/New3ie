0e4a9a9ce29ea5ede7a95ceb5789b9460eab4c26
New3ie <85528570+New3ie@users.noreply.github.com>
2021-06-12 17:59:02 +0700

    Create crd.sh

---
+++ crd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+echo -e '\e\nUntuk Chrome remote desktop pergi ke: \e\n[33mhttps://remotedesktop.google.com/headless  \e\n[mCopy perintah Debian Linux\e\n'
