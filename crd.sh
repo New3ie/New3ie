8bfb0853409fa94adc8ca991fc82f9525f9ccf68
New3ie <85528570+New3ie@users.noreply.github.com>
2021-06-12 19:52:24 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -10,4 +10,8 @@ sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
+wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
+tar -xvf xdm-setup-7.2.11.tar.xz
+chmod +x install.sh
+sudo ./install.sh
 echo -e '\e\nUntuk Chrome remote desktop pergi ke: \e\n[33mhttps://remotedesktop.google.com/headless  \e\n[mCopy perintah Debian Linux\e\n'
